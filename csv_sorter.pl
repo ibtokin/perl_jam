ccd80981d2b8c870c14a0ded177a43dfa30059ff
Adan <adanasandoval@gmail.com>
2014-10-23 18:01:50 -0500

    Got it sorting. Just needs a little cleaning up.

--- csv_sorter.pl
+++ csv_sorter.pl
@@ -3,17 +3,17 @@ use strict;
 use warnings;
 
 my $input = $ARGV[0] or die "You must specify a CSV file as a command line argument!\n";
+
 my $start = time();
 open(my $data, '<', $input) or die "Error opening $input!\nCheck your file type.\n";
-open my $fh, '>', "output_csv.txt" or die "Can't open output_csv.txt!";
+open my $fh, '>', "output.csv" or die "Can't open $input!";
+
 my $first_line = <$data>;
 my @header = split ",", $first_line;
-my %hash;
-print "\n", "The header row is: ", "@header";
 chomp @header;
+print "\n", "The header row is: ", "@header\n";
 my @order = join( ',', @header);
 @order = sort { lc($a) cmp lc($b) } @header; # 123-abc sort
-print "Key: ", "@order\n\n";
 foreach (@order){
     print $fh "$_";
 }
@@ -25,17 +25,21 @@ while (my $line = <$data>) {
 
     # Regex Hat Trick:
     if (grep( ((/^[0-9]*$/) && !(/^[a-zA-Z]*$/)), @row) ) {
-        foreach (@row) {
-            $_ =~ s/0*(\d+)/$1/;
+        foreach (@row) { #^if ints, no alphabetical:
+            $_ =~ s/0*(\d+)/$1/; #remove trailing zeroes
         }
     }
-    my @to_sort = join( ',', @row);
-    foreach (@to_sort){
-        #sorting stuff....
-        print $fh "SORTED:  ", "$_\n";
+    my @aa = @header;
+    my @bb = @row;
+    my @idx = sort { $aa[$a] cmp $aa[$b] } 0 .. $#aa;
+    @aa = @aa[@idx];
+    @bb = @bb[@idx];
+    print $fh "@aa\n";
+    #This is where we have to sort
+    #We have to sort the array '@row'
+    my $sorted = join( ',', @bb);
+    print $fh $sorted, "\n";
     }
-
-}
 
 close $fh;
 my $end = time();
