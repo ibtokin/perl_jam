5c8b36e5b9c36a19cd818c55b21488587195f294
Adan <adanasandoval@gmail.com>
2014-10-23 11:10:50 -0500

    almost

--- csv_sorter.pl
+++ csv_sorter.pl
@@ -9,39 +9,39 @@ print "\n";
 
 open(my $data, '<', $input) or die "Error opening $input!\nCheck your file type.\n";
 open my $fh, '>', "output_csv.txt" or die "Can't open output_csv.txt!";
-my @key;
 my $first_line = <$data>;
 my @header = split ",", $first_line;
 my %hash;
 my @sorted_row;
+my @key;
 
 print "The header row is: ", "@header";
 
 while (my $line = <$data>) {
     chomp $line;
     my @row = split ",", $line; # Regex Hat Trick:
+        #do stuff!
     if (grep( ((/^[0-9]*$/) && !(/^[a-zA-Z]*$/)), @row) ) { #if digit and not a-z
         foreach (@row) {
-            $_ =~ s/0*(\d+)/$1/; #remove leading zeros from each element
-            if (@key) {
-                print "There's a key!\n", "@key\n";
-                #do stuff!
-                my @index = sort { $key[$a]  <=> $key[$b]} 0 .. $#key;
-                @key = @key[@index];
-                @row = @row[@index];
-                print "sorted row: @row\n";
-                #
-            }
-            else {
-                print "there's no key! :(   \n";
-                @key = sort { lc($a) cmp lc($b) } @row;
-                print "Key assigned:  @key\n";
-                #default sort: upper/lower case equal, integers lead
-            }
-        }
+            $_ =~ s/0*(\d+)/$1/;
+            #remove leading zeros from each element in the row
+            my @aa = @header;
+            my @bb = @row;
+            my @index = sort { $key[$a] <=> $key[$b] } 0 .. $#key;
+            @key = @key[@index];
+            @sorted_row = @row[@index];
+            @hash{@key} = @sorted_row;
+            my $new_line = join( ",", @hash{@key});
+            print $fh "$new_line\n";
 
+        }
     }
 }
 close $fh;
 my $end = time();
 print "Time elapsed: ", $end-$start, " seconds\n\n";
+
+
+
+
+
