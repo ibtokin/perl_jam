cb897aff0121e50691730955f6d7936129363e20
Adan <adanasandoval@gmail.com>
2014-10-23 02:20:00 -0500

    getting close! Break time

--- csv_sorter.pl
+++ csv_sorter.pl
@@ -2,38 +2,32 @@
 use strict;
 use warnings;
 
-
 my $input = $ARGV[0] or die "You must specify a CSV file as a command line argument!\n";
 my @sorted_list;
-
-open(my $data, '<', $input) or die "Error opening $input!\nCheck your file type and try again.\n";
-
-print "\n";
-print "Original values:\n";
-while (my $row = <$data>) {
-    my @values = split ",", $row;
-    print @values;
-}
 
 print "\n";
 print "Sorted rows:";
 print "\n";
-open(my $sdata, '<', $input) or die "Error opening $input!\nCheck your file type and try again.\n";
-while (my $line = <$sdata>) {
+open(my $data, '<', $input) or die "Error opening $input!\nCheck your file type and try again.\n";
+#open our new csv file:
+open my $fh, '>', "output_csv.txt" or die "Can't open output_csv.txt!";
+while (my $line = <$data>) {
     chomp $line;
     my @list = split ",", $line;
+    if (grep( /^[0-9]*$/, @list)) {
+        if (!grep( /^[a-zA-Z]*$/, @list)) {
+            print "only numbers!";
+            foreach (@list) {
+                $_ =~ s/0*(\d+)/$1/;
+            }
+        }
+    }
     @sorted_list = sort { lc($a) cmp lc($b) } @list;
-    foreach(@sorted_list) {
-        print "$_,";
+    print join( ',', @sorted_list);
+    foreach (@sorted_list) {
+        print $fh "$_\n";
     }
     print "\n";
 }
+close $fh;
 print "\n";
-
-
-
-
-
-
-
-
