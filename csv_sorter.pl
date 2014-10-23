42a1143cc480a90cebba5b6172e0fd1fbe957f61
Adan <adanasandoval@gmail.com>
2014-10-23 04:49:35 -0500

    isolated header, working on hash map.

--- csv_sorter.pl
+++ csv_sorter.pl
@@ -6,22 +6,24 @@ my $input = $ARGV[0] or die "You must specify a CSV file as a command line argum
 my @sorted_list;
 
 print "\n";
-print "Sorted rows:";
-print "\n";
-open(my $data, '<', $input) or die "Error opening $input!\nCheck your file type and try again.\n";
-#open our new csv file:
+
+open(my $data, '<', $input) or die "Error opening $input!\nCheck your file type.\n";
 open my $fh, '>', "output_csv.txt" or die "Can't open output_csv.txt!";
+my $first_line = <$data>;
+my @header = split ",", $first_line;
+print "header IIIISSSSS....... ---> ";
+print "@header\n";
+print "Sorted rows:\n";
+
 while (my $line = <$data>) {
     chomp $line;
     my @list = split ",", $line; # Regex Hat Trick:
-    if (grep( /^[0-9]*$/, @list)) { # if 0-9,
-        if (!grep( /^[a-zA-Z]*$/, @list)) { # but no Aa-Zz..
-            foreach (@list) {
-                $_ =~ s/0*(\d+)/$1/; #remove leading zeros from elements
-            }
-        }
-    }
-    #default sort, upper/lower case equal, integers lead
+    if (grep( ((/^[0-9]*$/) && !(/^[a-zA-Z]*$/)), @list) ) {
+        foreach (@list) {
+            $_ =~ s/0*(\d+)/$1/;
+        }# if integers and not alphabetical chars
+    }#remove leading zeros from elements.
+    #default sort: upper/lower case equal, integers lead
     @sorted_list = sort { lc($a) cmp lc($b) } @list;
     @sorted_list = join( ',', @sorted_list);
     print @sorted_list;
