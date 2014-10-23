d99bfa8ee2171d73174b0d1e4967a62f7b21c9b5
Adan <adanasandoval@gmail.com>
2014-10-23 14:15:38 -0500

    just about there

--- csv_sorter.pl
+++ csv_sorter.pl
@@ -1,47 +1,42 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use List::MoreUtils qw(mesh);
 
 my $input = $ARGV[0] or die "You must specify a CSV file as a command line argument!\n";
 my $start = time();
-print "\n";
-
 open(my $data, '<', $input) or die "Error opening $input!\nCheck your file type.\n";
 open my $fh, '>', "output_csv.txt" or die "Can't open output_csv.txt!";
 my $first_line = <$data>;
 my @header = split ",", $first_line;
 my %hash;
-my @sorted_row;
-my @key;
-
-print "The header row is: ", "@header";
+print "\n", "The header row is: ", "@header";
+chomp @header;
+my @order = join( ',', @header);
+@order = sort { lc($a) cmp lc($b) } @header; # 123-abc sort
+print "Key: ", "@order\n\n";
+foreach (@order){
+    print $fh "$_";
+}
+print $fh "\n";
 
 while (my $line = <$data>) {
     chomp $line;
-    my @row = split ",", $line; # Regex Hat Trick:
-        #do stuff!
-    if (grep( ((/^[0-9]*$/) && !(/^[a-zA-Z]*$/)), @row) ) { #if digit and not a-z
+    my @row = split ",", $line;
+
+    # Regex Hat Trick:
+    if (grep( ((/^[0-9]*$/) && !(/^[a-zA-Z]*$/)), @row) ) {
         foreach (@row) {
             $_ =~ s/0*(\d+)/$1/;
-            #remove leading zeros from each element in the row
-            my @aa = @header;
-            my @bb = @row;
-            my @index = sort { $key[$a] <=> $key[$b] } 0 .. $#key;
-            @key = @key[@index];
-            @sorted_row = @row[@index];
-            @hash{@key} = @sorted_row;
-            my $new_line = join( ",", @hash{@key});
-            print $fh "$new_line\n";
-
         }
     }
+    my @to_sort = join( ',', @row);
+    foreach (@to_sort){
+        #sorting stuff....
+        print $fh "SORTED:  ", "$_\n";
+    }
+
 }
+
 close $fh;
 my $end = time();
 print "Time elapsed: ", $end-$start, " seconds\n\n";
-
-
-
-
-
