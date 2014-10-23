4769dde8c0839573b5cd960591128fa7d159d8b0
Adan <adanasandoval@gmail.com>
2014-10-23 06:37:40 -0500

    All that's left is to map it.

--- csv_sorter.pl
+++ csv_sorter.pl
@@ -3,35 +3,41 @@ use strict;
 use warnings;
 
 my $input = $ARGV[0] or die "You must specify a CSV file as a command line argument!\n";
-my @sorted_list;
-
+my $start = time();
 print "\n";
 
 open(my $data, '<', $input) or die "Error opening $input!\nCheck your file type.\n";
 open my $fh, '>', "output_csv.txt" or die "Can't open output_csv.txt!";
+my @order;
 my $first_line = <$data>;
 my @header = split ",", $first_line;
-print "header IIIISSSSS....... ---> ";
-print "@header\n";
-print "Sorted rows:\n";
+my %hash;
+my @sorted_row;
+
+print "The header row is: ", "@header";
 
 while (my $line = <$data>) {
     chomp $line;
-    my @list = split ",", $line; # Regex Hat Trick:
-    if (grep( ((/^[0-9]*$/) && !(/^[a-zA-Z]*$/)), @list) ) {
-        foreach (@list) {
+    my @row = split ",", $line; # Regex Hat Trick:
+    if (grep( ((/^[0-9]*$/) && !(/^[a-zA-Z]*$/)), @row) ) {
+        foreach (@row) {
             $_ =~ s/0*(\d+)/$1/;
-        }# if integers and not alphabetical chars
-    }#remove leading zeros from elements.
-    #default sort: upper/lower case equal, integers lead
-    @sorted_list = sort { lc($a) cmp lc($b) } @list;
-    @sorted_list = join( ',', @sorted_list);
-    print @sorted_list;
+        }
+        if (!(@order)) {
+                @order = join( ',', @row);
+                @order = sort { lc($a) cmp lc($b) } @row;
+                print "The key being used is: ", "@order\n\n";
+        }
+            # if integers and not alphabetical chars
+    }       #remove leading zeros from elements
 
-    foreach (@sorted_list){
+    #default sort: upper/lower case equal, integers lead
+    #    @sorted_row;#what our hash will sort
+    @sorted_row = join( ',', @row);
+    foreach (@sorted_row){
         print $fh "$_\n";
     }
-    print "\n";
 }
 close $fh;
-print "\n";
+my $end = time();
+print "Time elapsed: ", $end-$start, " seconds\n\n";
