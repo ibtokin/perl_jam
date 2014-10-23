65279785444aaf7486d9f913e9d42b7bd4ddbba8
Adan <adanasandoval@gmail.com>
2014-10-23 04:04:14 -0500

    Close, but no cigar.

--- csv_sorter.pl
+++ csv_sorter.pl
@@ -13,18 +13,20 @@ open(my $data, '<', $input) or die "Error opening $input!\nCheck your file type 
 open my $fh, '>', "output_csv.txt" or die "Can't open output_csv.txt!";
 while (my $line = <$data>) {
     chomp $line;
-    my @list = split ",", $line;
-    if (grep( /^[0-9]*$/, @list)) {
-        if (!grep( /^[a-zA-Z]*$/, @list)) {
-            print "only numbers!";
+    my @list = split ",", $line; # Regex Hat Trick:
+    if (grep( /^[0-9]*$/, @list)) { # if 0-9,
+        if (!grep( /^[a-zA-Z]*$/, @list)) { # but no Aa-Zz..
             foreach (@list) {
-                $_ =~ s/0*(\d+)/$1/;
+                $_ =~ s/0*(\d+)/$1/; #remove leading zeros from elements
             }
         }
     }
+    #default sort, upper/lower case equal, integers lead
     @sorted_list = sort { lc($a) cmp lc($b) } @list;
-    print join( ',', @sorted_list);
-    foreach (@sorted_list) {
+    @sorted_list = join( ',', @sorted_list);
+    print @sorted_list;
+
+    foreach (@sorted_list){
         print $fh "$_\n";
     }
     print "\n";
