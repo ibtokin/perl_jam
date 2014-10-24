42adb0efd786af7193e0ddb6c87e95c90ebf684d
Adan <adanasandoval@gmail.com>
2014-10-23 19:04:59 -0500

    All done!

--- csv_sorter.pl
+++ csv_sorter.pl
@@ -1,43 +1,41 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use Time::HiRes;
 
 my $input = $ARGV[0] or die "You must specify a CSV file as a command line argument!\n";
+open my $data, '<', $input or die "Error opening $input!\nCheck your file type.\n";
+open my $fh, '>', "output.csv" or die "error opening output.csv!\n";
+my $start = time;
 
-my $start = time();
-open(my $data, '<', $input) or die "Error opening $input!\nCheck your file type.\n";
-open my $fh, '>', "output.csv" or die "Can't open $input!";
-
-my $first_line = <$data>;
-my @header = split ",", $first_line;
+my @header = split( ",", <$data>);
 chomp @header;
-print "\n", "The header row is: ", "@header\n";
+print "\n", "Mapping indices according to: ", "\n", "@header\n\n";
 my @order = join( ',', @header);
-@order = sort { lc($a) cmp lc($b) } @header; # 123-abc sort
-print $fh join( ',', @header);
+@order = sort { lc($a) cmp lc($b) } @header; # 123-abc natural sort
+
+print $fh join( ',', @order);
 print $fh "\n";
 
 while (my $line = <$data>) {
     chomp $line;
-    my @row = split ",", $line;
-
+    my @row = split (",", $line);
     # Regex Hat Trick:
     if (grep( ((/^[0-9]*$/) && !(/^[a-zA-Z]*$/)), @row) ) {
-        foreach (@row) { #^if ints, no alphabetical:
-            $_ =~ s/0*(\d+)/$1/; #remove trailing zeroes
+        foreach (@row) { #if ints and not alphabetical..
+            $_ =~ s/0*(\d+)/$1/; #remove trailing zeros
         }
     }
+    #create new vars to simplify the mapping alg
     my @aa = @header;
     my @bb = @row;
-    my @idx = sort { $aa[$a] cmp $aa[$b] } 0 .. $#aa;
-    @aa = @aa[@idx];
-    @bb = @bb[@idx];
-    #This is where we have to sort
-    #We have to sort the array '@row'
+    my @index = sort { $aa[$a] cmp $aa[$b] } 0 .. $#aa;
+    @aa = @aa[@index];
+    @bb = @bb[@index];
     my $sorted = join( ',', @bb);
     print $fh $sorted, "\n";
-    }
+}
 
 close $fh;
-my $end = time();
-print "Time elapsed: ", $end-$start, " seconds\n\n";
+my $end = time;
+print "Time elapsed: ", ($end - $start), " milliseconds\n\n";
