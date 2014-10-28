985fcf4762a4e8a782d4ebe1d418f9c6a3efde06
Adan <adanasandoval@gmail.com>
2014-10-28 12:06:08 -0500

    Create singe_csv_sort.pl

---
+++ singe_csv_sort.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+BEGIN {
+package resstrs;
+sub new {
+    my $self  = {};
+    sub str1 { return "string1\n";}
+    sub str2 { return "string2";} 
+    sub fname { return "output.csv";} 
+    sub strError { my ($self,$m)=@_; return "Exit, error. L=$m\n"; }
+    bless($self);
+    return $self;
+}       
+}
+
+my $strO=resstrs->new; 
+
+print $0 ." ". $strO->str1;
+
+my %hash=();
+$hash{ $strO->str2 } = "A suggestion"; 
+
+open my $fh, '>', $strO->fname or die $strO->strError(__LINE__);
